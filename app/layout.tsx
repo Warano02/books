1405da315905803fd5b00418e0c9cac9a28f0025
Warano02 <carineteoi@gmail.com>
2026-02-28 07:34:58 +0100

    dummy nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Serif, Mona_Sans } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ["latin"],
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${monoSans.variable} ${ibmPlexSerif.variable} relative font-sans antialiased`}
       >
+        <Navbar/>
         {children}
       </body>
     </html>
